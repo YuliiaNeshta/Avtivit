477fd6a9e0449a83994d60309db9b98faf775df5
YuliiaNeshta <62185405+YuliiaNeshta@users.noreply.github.com>
2020-10-04 13:19:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//JS-ФУНКЦИЯ ОПРЕДЕЛЕНИЯ ПОДДЕРЖКИ WEBP
+function testWebP(callback) {
+
+var webP = new Image();
+webP.onload = webP.onerror = function () {
+callback(webP.height == 2);
+};
+webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+if (support == true) {
+document.querySelector('body').classList.add('webp');
+}else{
+document.querySelector('body').classList.add('no-webp');
+}
+});
+var buttonShow = document.querySelector('.btn-visible');
+var textShow = document.querySelector('.research__info');
+
+
+buttonShow.onclick = function(){
+		textShow.classList.toggle('research__info--visible');
+	}
+
+
+
+
